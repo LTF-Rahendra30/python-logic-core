8f8d8c0639c17e9ba477e46b562cc98a2835845f
0×007rhn64LTF2700E <kecuu91@gmail.com>
2026-02-12 13:45:29 +0700

    Implement basic while loop in While_loop_basic.py
    Add a basic while loop that counts from 0 to 4.

---
+++ 01_loop/While_loop/While_loop_basic.py
@@ -0,0 +1,10 @@
+#Basic While Loop
+
+#initialized variable 
+count = 0
+
+#loop While count is lees than 5
+while count < 5:
+  print(count)
+  count =+ 1 #Update the counter withn the loop
+print("Loop Finished!) 
