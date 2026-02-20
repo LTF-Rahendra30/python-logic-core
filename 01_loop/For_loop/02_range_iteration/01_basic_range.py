d50483d994d4ac757ff244c4130e65b2a07c1a71
0×007rhn64LTF2700E <kecuu91@gmail.com>
2026-02-20 18:54:26 +0700

    Add files via upload

---
+++ 01_loop/For_loop/02_range_iteration/01_basic_range.py
@@ -0,0 +1,14 @@
+#Today i'm start learn for loop range in pyhon
+
+for i in range(6): # loop num for 0 to 5
+    print(i)
+
+print("===========================")
+
+for i in range(1,6): # loop num for 1 to 5
+    print(i)
+
+print("===========================")
+
+for i in range(0,11,2): # loop num for 1 to 10, but step 2 number
+    print(i)
