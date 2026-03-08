de4682499cebe8ad97fba4c2ae513900d5cca331
0×007rhn64LTF2700E <kecuu91@gmail.com>
2026-03-08 17:46:12 +0700

    Update 04_execercise_range.py

--- 01_loop/For_loop/02_range_iteration/04_execercise_range.py
+++ 01_loop/For_loop/02_range_iteration/04_execercise_range.py
@@ -1,1 +1,42 @@
-qwhdhqwuh
+"""
+04_exercise_range.py
+
+
+Latihan Range Iteration
+Kerjakan tanpa melihat solusi.
+"""
+
+
+# Exercise 1
+# Buat list [1,2,3,4,5]
+# Gunakan range() untuk menggandakan setiap nilai
+# Expected result:
+# [2,4,6,8,10]
+
+
+
+
+# Exercise 2
+# Print angka 10 sampai 1 menggunakan range()
+
+
+
+
+# Exercise 3
+# Diberikan list:
+numbers = [3,5,7,9]
+
+
+# Hitung totalnya menggunakan loop + range()
+# Jangan gunakan fungsi sum()
+
+
+
+
+# Exercise 4
+# Diberikan list:
+data = [2,4,6,8]
+
+
+# Buat cumulative sum seperti ini:
+# [2,6,12,20]
