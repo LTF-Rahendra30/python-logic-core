1e78cabea9c5a221eb37c1052b2d1a1a0327935c
0×007rhn64LTF2700E <kecuu91@gmail.com>
2026-05-06 11:15:50 +0700

    Add files via upload

---
+++ 02_functions/02_core_logic/02_scope.py
@@ -0,0 +1,61 @@
+"""
+The global variabels & Local variabels
+"""
+
+# Changing global variabels inside function but This is eror, because global variabels can't be changed
+"""
+total = 0
+def num():
+    total = total + x
+
+
+num(5)
+"""
+
+# GLOBAL Variabels can be read in the function
+x = 10
+
+
+def get():
+    print(x)
+
+
+get()
+"""
+The output is value of the x variabels : 10
+"""
+
+"""
+def num():
+    y = 10
+
+
+num() # This output is a: 10
+print(y) # is undefined, because y is local variabels in the loop
+
+"""
+
+# variabels names are the seme between GLOBAL Var and LOCAL, then this:
+age = 17  # Global Var
+
+
+def young():
+    age = 18  # Local Var
+    print(age)  # Print Var
+
+
+young()
+"""
+The ouput is a : 18, because Global var are overwritten by local var
+"""
+# The function that rely on globals var and then change the global var in the loop, the like this:
+
+name = "andrew"
+
+
+def change_name():
+    name = "Barley"
+
+
+print(change_name())
+"""The output is : NONE,"""
