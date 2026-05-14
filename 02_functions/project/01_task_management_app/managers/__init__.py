f23059a3033d76cfa34be2bbb6cfa424ccc0250f
0×007rhn64LTF2700E <kecuu91@gmail.com>
2026-05-14 17:36:25 +0700

    Add files via upload

---
+++ 02_functions/project/01_task_management_app/managers/__init__.py
@@ -0,0 +1,10 @@
+from managers.user_manager import register_user, get_users, get_user_by_username
+from managers.task_managers import (
+    create_task,
+    get_all_task,
+    get_task_by_category,
+    get_task_by_user,
+    get_task_by_status,
+    delete_task,
+    mark_task_complate
+)
