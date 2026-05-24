e4768fb7e7521718635f163b1418af277b391186
LTF-Rahendra30 <kecuu91@gmail.com>
2026-05-24 13:08:23 +0700

    feat: Add duplicate username and email check functions

--- project/wallet_crypto_tracker/validators/user_validators.py
+++ project/wallet_crypto_tracker/validators/user_validators.py
@@ -7,3 +7,16 @@ def validate_username(username):
 
 def validate_password(password):
     return len(password) >=8
+
+def check_username_duplicate(username,users_list):
+    for usr in users_list:
+        if usr["name"] == username:
+            return False
+    return True
+
+def check_email_duplicate(email,users_list):
+    for eml in users_list:
+        if eml["email"] == email:
+            return False
+    return True
+
