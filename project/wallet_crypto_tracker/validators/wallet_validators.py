819505a2c75295da182af3adb54654692247c75e
LTF-Rahendra30 <kecuu91@gmail.com>
2026-05-25 13:45:29 +0700

    feat: Make a new file  wallet_validators

---
+++ project/wallet_crypto_tracker/validators/wallet_validators.py
@@ -0,0 +1,13 @@
+def validate_coin_type(coin_type):
+    return coin_type in ["Bitcoin", "Ethereum", "Solana"]
+
+def validate_wallet_addres(addres):
+    return len(addres) >= 20
+
+def check_wallet_duplicate(address,users_list):
+    for user in users_list:
+        for adrs in user["wallets"]:
+            if adrs["address"] == address:
+                return False
+    return True
+            
