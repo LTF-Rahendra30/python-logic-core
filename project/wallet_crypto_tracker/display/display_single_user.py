8658bf80828fe4cce5788791bf5ee303428a4ac0
LTF-Rahendra30 <kecuu91@gmail.com>
2026-05-25 12:30:09 +0700

    feat: Make a display function

---
+++ project/wallet_crypto_tracker/display/display_single_user.py
@@ -0,0 +1,21 @@
+
+
+# Clean Display Function
+def clean_display_single_user(user):
+    print(f"""
+Username: {user['username']}
+Email: {user['email']}
+Wallets: {len(user['wallets'])}
+─────────────────────
+""")
+
+
+def display_multiple_users(users_list):
+    if not users_list:
+        print("No users found")
+        return
+    
+    print("==== ALL USERS ====")
+
+    for user in users_list:
+        clean_display_single_user(user)
