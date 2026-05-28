e7357bc3379d9510e9cf5c83b20a26db7ec24f33
LTF-Rahendra30 <kecuu91@gmail.com>
2026-05-28 11:50:22 +0700

    fix: fix a bug inndentation eror

--- project/wallet_crypto_system/main.py
+++ project/wallet_crypto_system/main.py
@@ -4,6 +4,7 @@ from managers import register_user,get_user_by_username,get_all_users,users
 from managers import add_wallet,get_wallets_by_user,get_wallet_by_id,calculate_user_total_balance
 
 
+
 print(register_user("joko","joko@mail.com","joko1234567",users))
 
 print(add_wallet("joko","BITCOIN","11bbabbc213370099effb32bfe31699",users))
@@ -11,4 +12,6 @@ print(add_wallet("joko","BITCOIN","11bbabbc213370099effb32bfe31699",users))
 print(add_wallet("joko","solana","22eeb3987abe3345cc231fffcdee337efb2",users))
 
 print(get_wallets_by_user("joko",users))
+
+
 print(calculate_user_total_balance("joko",users))
--- project/wallet_crypto_system/managers/transaction_managers.py
+++ project/wallet_crypto_system/managers/transaction_managers.py
@@ -5,14 +5,11 @@ from validators import (
     validate_date,
 )
 
-from d
 
 def add_transaction(
-    username, wallet_id, tx_type, amount, description, date, users_list
+    username, wallet_id,tx_type, amount, description, date, users_list
 ):
     clean_username = username.strip().lower()
-
-
     found_user = None
     # Find username
     for user in users_list:
@@ -26,8 +23,11 @@ def add_transaction(
     # Find wallet
     for wallet in found_user["wallets"]:
         if wallet["wallet_id"] == wallet_id:
-            return True
-    return False, "Not Found"
+            return (True,wallet)
+        else:
+            return (False, "Wallet Not Found")
 
+    # Transaction Data
+    valid_tx_type = validate_tx_type(tx_type) 
 
         
