bb021a7a2b12ac231e4a2f78cacf7d90c523dd05
LTF-Rahendra30 <kecuu91@gmail.com>
2026-05-28 11:32:41 +0700

    feat: Make a loop that find wallet and user

--- project/wallet_crypto_system/managers/transaction_managers.py
+++ project/wallet_crypto_system/managers/transaction_managers.py
@@ -5,19 +5,29 @@ from validators import (
     validate_date,
 )
 
+from d
 
 def add_transaction(
     username, wallet_id, tx_type, amount, description, date, users_list
 ):
     clean_username = username.strip().lower()
-    found_user = None
 
+
+    found_user = None
+    # Find username
     for user in users_list:
         if user["username"] == clean_username:
-            user = found_user
+            found_user = user
             break
     
     if found_user is None:
         return (False, f"User {clean_username} not found")
+    
+    # Find wallet
+    for wallet in found_user["wallets"]:
+        if wallet["wallet_id"] == wallet_id:
+            return True
+    return False, "Not Found"
+
 
         
