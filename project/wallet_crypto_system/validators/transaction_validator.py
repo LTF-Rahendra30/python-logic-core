2301ddf4ff7579fcf03fc1c029789b61a36ed97f
LTF-Rahendra30 <kecuu91@gmail.com>
2026-05-28 10:47:38 +0700

    feat: Add a validate date function

--- project/wallet_crypto_system/validators/transaction_validator.py
+++ project/wallet_crypto_system/validators/transaction_validator.py
@@ -4,3 +4,20 @@ def validate_tx_type(tx_type):
 def validate_amount(amount):
     return amount > 0
 
+def validate_date(date):
+    parts = date.split("-")
+
+    #  Check lenght
+    if len(parts) !=3:
+        return False
+    
+    # Check digit
+    for part in parts:
+        if not part.isdigit():
+            return False
+        
+    # Check date  format
+    if len(part[0]) != 4 or len(part[1]) != 2 or len(part[2]) != 2:
+        return False
+    
+    return True
