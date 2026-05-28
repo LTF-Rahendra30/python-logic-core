8b02f12dc7047a1246e10267c2d01dfda4ae7888
LTF-Rahendra30 <kecuu91@gmail.com>
2026-05-28 11:10:00 +0700

    feat: Make add transaction function in managers folder

---
+++ project/wallet_crypto_system/managers/transaction_managers.py
@@ -0,0 +1,23 @@
+from validators import (
+    validate_tx_type,
+    validate_amount,
+    validate_sufficient_balance,
+    validate_date,
+)
+
+
+def add_transaction(
+    username, wallet_id, tx_type, amount, description, date, users_list
+):
+    clean_username = username.strip().lower()
+    found_user = None
+
+    for user in users_list:
+        if user["username"] == clean_username:
+            user = found_user
+            break
+    
+    if found_user is None:
+        return (False, f"User {clean_username} not found")
+
+        
