838cb482833b8bc7f9ed10df61fbcc36c2c76561
LTF-Rahendra30 <kecuu91@gmail.com>
2026-05-31 10:40:33 +0700

    feat: Set up a main file to try

--- project/wallet_crypto_system/main.py
+++ project/wallet_crypto_system/main.py
@@ -1,27 +1,43 @@
 
-from managers import register_user,get_user_by_username,get_all_users,users
+from managers import (
+    # Register
+    register_user,get_user_by_username,get_all_users,users,
+    
+    # Wallet Managers
+    add_wallet,get_wallets_by_user,get_wallet_by_id,calculate_user_total_balance,
 
-from managers import add_wallet,get_wallets_by_user,get_wallet_by_id,calculate_user_total_balance
-
-from managers import add_transaction,get_transaction,filter_transactions_by_type,filter_transactions_by_date,filter_transaction_by_hash
-
-print(register_user("joko","joko@mail.com","joko1234567",users))
-
-# Add wallet
-print(add_wallet("joko","BITCOIN","11bbabbc213370099effb32bfe31699",users))
-print(add_wallet("joko","solana","22eeb3987abe3345cc231fffcdee337efb2",users))
-
-# Add transaction
-print(add_transaction("joko",1,"in",0.1,"From CEX","2026-05-21",users))
-print(add_transaction("joko",1,"in",1,"From CEX","2026-05-25",users))
-print(add_transaction("joko",1,"out",0.5,"Send to bahlil","2026-05-31",users))
-print(add_transaction("joko",2,"in",0.3,"from bahlil","2026-05-31",users))
-
-# Get Data
-get_wallets_by_user("joko",users)
-get_transaction("joko",2,users)
-filter_transactions_by_type("joko",1,"out",users)
-filter_transaction_by_hash("joko","0x4c6bed350fe256a5f1303795cd7fa8a4cac5c79d702a137f8036cbc41f88beca",users)
+    # Transaction Managers
+    add_transaction,get_transaction,filter_transactions_by_type,filter_transactions_by_date,filter_transaction_by_hash
+    )
 
 
-# print(calculate_user_total_balance("joko",users))
+def main():
+    print(register_user("Bob","Bob@mail.com","joko1234567",users))
+
+    print("\n" + "=" * 50 + "\n")
+
+    # Add wallet
+    print(add_wallet("bob","BITCOIN","11bbabbc213370099effb32bfe31699",users))
+    print(add_wallet("bob","solana","22eeb3987abe3345cc231fffcdee337efb2",users))
+
+    print("\n" + "=" * 50 + "\n")
+
+    # Add transaction
+    print(add_transaction("bob",1,"in",0.1,"From CEX","2026-05-21",users))
+    print(add_transaction("bob",1,"in",1,"From CEX","2026-05-25",users))
+    print(add_transaction("bob",1,"out",0.5,"Send to dony","2026-05-31",users))
+    print(add_transaction("bob",2,"in",0.3,"from dony","2026-05-31",users))
+
+    print("\n" + "=" * 50 + "\n")
+
+    # Get Data
+    get_wallets_by_user("bob",users)
+    get_transaction("bob",2,users)
+    filter_transactions_by_type("bob",1,"out",users)
+    filter_transaction_by_hash("bob","0x8a665ce8c4a01aebf6c6c3c4a5d5eab50b3cfdb4b1e419c46e383a22ecd35b3f",users)
+
+    # Calculate total balace
+    print(calculate_user_total_balance("bob",users))
+
+if __name__ == "__main__":
+    main()
