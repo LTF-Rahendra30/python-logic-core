85fe6aba10c8862690dc047442b8e35d91a2955e
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-01 20:47:51 +0700

    feat: Add the atributte i class of a Car

--- 04 _object_oriented_programming/01_basic_class/01_basic_class.py
+++ 04 _object_oriented_programming/01_basic_class/01_basic_class.py
@@ -2,6 +2,8 @@
 # The Class of Car
 class Car:
     # Artibute of the Car
-    def __init__(self,color,max_speed):
+    def __init__(self,color,fuel,mileage,max_speed):
         self.color = color
+        self.fuel = fuel
+        self.mileage = mileage
         self.max_speed = max_speed
