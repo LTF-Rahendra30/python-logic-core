748cf9d1f21f73d9d71c281e83b801719a8b3a5b
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-01 21:05:55 +0700

    feat: Add a method

--- 04 _object_oriented_programming/01_basic_class/01_basic_class.py
+++ 04 _object_oriented_programming/01_basic_class/01_basic_class.py
@@ -2,8 +2,15 @@
 # The Class of Car
 class Car:
     # Artibute of the Car
-    def __init__(self,color,fuel,mileage,max_speed):
+    def __init__(self,color,fuel,mileage,speed):
         self.color = color
         self.fuel = fuel
         self.mileage = mileage
-        self.max_speed = max_speed
+        self.speed = speed
+        self.cost = 0
+    
+    def calculate_cost_fuel(self,speed,fuel,mileage):
+        return self.cost (mileage / speed) * fuel
+
+car1 = Car("red",10,100,50)
+print(car1.cost)
