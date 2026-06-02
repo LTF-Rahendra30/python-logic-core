60f55ac083f80c741be9c8bf8c95f200a2d9a0dd
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-02 21:11:46 +0700

    feat: Updated the method in this class objcet

--- 04 _object_oriented_programming/01_basic_class/01_basic_class.py
+++ 04 _object_oriented_programming/01_basic_class/01_basic_class.py
@@ -2,8 +2,12 @@
 # The Class of Car
 class Car:
     # Artibute of the Car
-    def __init__(self,merk,color,max_speed):
+    def __init__(self,merk,color):
         self.merk = merk
         self.color = color
-        self.max_speed = max_speed
-        
+        self.max_speed = 0
+
+    # Method
+    def car_info(self,add_speed):
+        self.max_speed += add_speed
+        return (True, f"Car {self.merk} , now speed: {self.max_speed} km/h")
