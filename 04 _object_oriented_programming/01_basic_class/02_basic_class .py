0d8b2ea71442e3c43cd82c0d863a8f383d704d55
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-03 23:07:25 +0700

    feat: Add attributes and methods to Student class

--- 04 _object_oriented_programming/01_basic_class/02_basic_class .py
+++ 04 _object_oriented_programming/01_basic_class/02_basic_class .py
@@ -5,9 +5,22 @@ class University:
         self.acreditation = acreditation
         self.world_rank = world_rank
 
+# Without __init__
 class Student:
-    pass
+    name = ""
+    gpa = 0
 
+    def introduction(self):
+        print(f"Helo, my name: {self.name}")
 # Create Object
 university1 = University("NYC","A","20")
-print(type(university1)) # Type : CLass
+print(type(university1))# Type : CLass
+print(university1.location) # NYC
+print(university1.acreditation) # A
+print(university1.world_rank) # 20
+
+# Create manual atribut and method without definded atribute by __init__
+print("="* 100)
+student1 = Student()
+student1.name = "Bob"
+student1.introduction()
