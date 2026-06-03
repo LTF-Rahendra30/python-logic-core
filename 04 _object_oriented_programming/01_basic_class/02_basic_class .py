f6e7554a7d6f335872722e26bdaecc006a4d6ae3
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-03 22:44:19 +0700

    feat: Add atribute class

--- 04 _object_oriented_programming/01_basic_class/02_basic_class .py
+++ 04 _object_oriented_programming/01_basic_class/02_basic_class .py
@@ -1,8 +1,13 @@
 class University:
-    pass
+    # Add atribute
+    def __init__(self,location,acreditation,world_rank):
+        self.location = location
+        self.acreditation = acreditation
+        self.world_rank = world_rank
+
 class Student:
     pass
 
 # Create Object
-university1 = University()
+university1 = University("NYC","A","20")
 print(type(university1)) # Type : CLass
