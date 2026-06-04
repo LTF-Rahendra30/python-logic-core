a63876489da27182f7538e9565ec32e68942effc
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-04 22:46:12 +0700

    feat: Make a method can afford

--- 04 _object_oriented_programming/01_basic_class/04_excersie.py
+++ 04 _object_oriented_programming/01_basic_class/04_excersie.py
@@ -3,12 +3,18 @@ class User():
         self.name = name
         self.age = age
         self.balance = balance
+        self.price = 0
 
     def introduce_user(self):
-        print(f"Hello {self.name}")
+        print(f"Hello {self.name} | age: {self.age} | Balance: {self.balance}")
     
     def can_afford(self,price):
-        if price <= self.balance:
+        if self.price >= self.balance:
             return(False,"You cant buy something by your balance")
         self.balance -= price
         return (True,f"Success, your balance now at {self.balance}")
+
+# Create Object   
+user1 = User("Bob",12,10000)
+user1.introduce_user()
+print(user1.can_afford(1000))
