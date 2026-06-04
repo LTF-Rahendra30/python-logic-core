b12c8e8b26882f04e219a225ca2183a7655c5779
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-04 23:14:32 +0700

    fix: Correct balance check logic in can_afford method and remove unused price attribute

--- 04 _object_oriented_programming/01_basic_class/04_excersie.py
+++ 04 _object_oriented_programming/01_basic_class/04_excersie.py
@@ -19,7 +19,7 @@ class User():
         self.name = str(name)
         self.age = str(age)
         self.balance = int(balance)
-        self.price = 0
+        
 
         # Error handling
         if balance <= 0:
@@ -31,14 +31,15 @@ class User():
         print(f"Hello {self.name} | age: {self.age} | Balance: {self.balance}")
     
     def can_afford(self,price):
-        if self.price >= self.balance:
+        if price > self.balance:
             return(False,"You cant buy something by your balance")
-        self.balance -= price
-        return (True,f"Success, your balance now at {self.balance}")
+        else:
+            self.balance -= price
+            return (True,f"Success buy, your balance now at {self.balance}")
 
 # Create Object
 try:
-    user1 = User("Bob",12,0)
+    user1 = User("Bob",12,10000)
     user1.introduce_user()
     print(user1.can_afford(1000))
 except ValueError as e:
