d2f86d9f4a77978e4b4eb5596936c49628582c04
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-04 10:10:22 +0700

    feat: Add constructor example to demonstrate object creation

--- 04 _object_oriented_programming/01_basic_class/03_constuctor.py
+++ 04 _object_oriented_programming/01_basic_class/03_constuctor.py
@@ -13,4 +13,21 @@ student = Student()
 student.setup(400,"Bob")
 print(student.name)
 print(student.gpa)
+"""
+This is manual call object, because whitout constructor deffinition at the beginning
+"""
 
+
+print("=" * 50)
+
+# With Constructor
+class Student2():
+    def __init__(self,name,gpa):
+        self.name = name
+        self.gpa = gpa
+student = Student2("Andrew",300)
+print(student.name)
+print(student.gpa)
+"""
+Create object with constructor, without having to do it manually
+"""
