f0718ef6b9789481d7e623a400a6ecd2a3ba2e74
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-06 22:17:06 +0700

    feat: Add 1 method, add simple explanation and also Enhance User constructor documentation and clarify attribute purpose

--- 04 _object_oriented_programming/02_state_and_method/01_state_basics.py
+++ 04 _object_oriented_programming/02_state_and_method/01_state_basics.py
@@ -1,5 +1,17 @@
 class User:
-    def __init__(self,name,balance):
-        self.name = name
+    # Constructor
+    def __init__(self,name,balance): # Atributte
+        self.name = name 
         self.balance = balance
-        
+
+    """
+        self.name = name 
+        self.balance = balance
+
+        'name' and 'balance' This isnt only state, but atributtes holding data in the object and had lifecyle, until the data is changed and given behaviour in the method
+    
+    """
+
+    # Method
+    def get_info(self):
+        return f"Welcome: {self.name}"
