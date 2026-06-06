b431dee7c476a97314cbb84f127936dd61f28496
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-06 21:45:59 +0700

    fix: correct validation logic in constructor and update user age for testing

--- 04 _object_oriented_programming/01_basic_class/04_excersie.py
+++ 04 _object_oriented_programming/01_basic_class/04_excersie.py
@@ -17,10 +17,10 @@ class User():
     def __init__(self,name,age,balance):
     
         
-        # Error handling
-        if balance or age <= 0:
-            raise ValueError("Cant be zero or Negative")
         self.name = str(name)
+        # Error handling
+        if balance <0 or age <= 0:
+            raise ValueError("Cant be zero or Negative")
         self.age = int(age)
         self.balance = int(balance)
         
@@ -28,20 +28,15 @@ class User():
     def introduce_user(self):
         return f"Hello {self.name} | age: {self.age} | Balance: {self.balance}"
     
-    def can_afford(self,price):
-        if price > self.balance:
-            return(False,"You cant buy something by your balance")
-        return(True,"Your Eligible")
     def spend(self,amount):
-        if amount > self.balance:
+        if amount >= self.balance:
             return (False, "Your balance isnt enough")
         self.balance -= amount
         return (True,f"Success buy, your balance now at {self.balance}")
 # Create Object
 try:
-    user1 = User("Bob",1,10000)
+    user1 = User("Bob",12,10000)
     print(user1.introduce_user())
-    print(user1.can_afford(1000))
     print(user1.spend(2000))
 except ValueError as e:
     print(f"Errors for create objcet: {e}")
