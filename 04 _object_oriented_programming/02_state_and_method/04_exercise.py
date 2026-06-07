9dddd40b981836d0c724eb8279cf0663dfb4437e
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-07 08:21:05 +0700

    feat: Make method to show update state

--- 04 _object_oriented_programming/02_state_and_method/04_exercise.py
+++ 04 _object_oriented_programming/02_state_and_method/04_exercise.py
@@ -9,17 +9,26 @@ Simple sistem Bank Account, every account have:
 class BankAccount:
     def __init__(self,name,balance):
         self.name = name
-        self.balancen = balance
+        self.balance = balance
 
+    def get_info(self):
+        return f"Name: {self.name} | Balance: {self.balance}"
+    
     def deposit(self,amount):
         if amount <= 0:
             return False
-        self.balancen += amount
-        return True
+        self.balance += amount
+        return True, self.get_info()
     
     def withdraw(self,amount):
-        if amount > self.balancen or amount <= 0:
+        if amount > self.balance or amount <= 0:
             return False
-        self.balancen -= amount
+        self.balance -= amount
         return True
     
+    def transfer(self,target_account,amount):
+        if amount > self.balance or amount <= 0:
+            return False
+
+user1 = BankAccount("bob",100)
+print(user1.deposit(100))
