dcd515df09d01b8ce982c521f73ef593eacdabc2
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-08 23:34:09 +0700

    feat: Try manipulation state

--- 04 _object_oriented_programming/03_encapsulation/01_break_the_system.py
+++ 04 _object_oriented_programming/03_encapsulation/01_break_the_system.py
@@ -20,3 +20,27 @@ user2 = BankAccount("jerry",350)
 print("before: ",user1.balance)
 
 # Normal Usage
+user1.withdraw(50)
+print("After withdraw: ",user1.balance)
+
+"""
+The output:
+
+before:  100
+After withdraw:  50
+
+This is like a normal
+"""
+
+# Breaak The System
+user1.balance = -99999999
+print("Hacked balance:", user1.balance)
+"""
+The output:
+
+before:  100
+After withdraw:  50
+Hacked balance: -99999999  # The State could manipulation, and whitout validation, and also the method useless
+
+
+"""
