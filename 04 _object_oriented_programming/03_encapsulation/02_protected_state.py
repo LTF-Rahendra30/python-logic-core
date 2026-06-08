bd1907cbcc7731c3d1cf4e6b8c47a5b8e9f37a2b
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-08 23:43:31 +0700

    feat: Try creat object, but, the state of obcjet can still be manipulation

--- 04 _object_oriented_programming/03_encapsulation/02_protected_state.py
+++ 04 _object_oriented_programming/03_encapsulation/02_protected_state.py
@@ -1,7 +1,7 @@
 class BankAccount:
     def __init__(self,name,balance):
         self.name = name
-        self.balance = balance # Protacted
+        self._balance = balance # Protacted
 
     def deposit(self,amount):
         if amount <= 0:
@@ -14,3 +14,20 @@ class BankAccount:
             return False,None
         self._balance -= amount
         return True
+    
+acc = BankAccount("Bob", 100)
+
+print("Before:", acc._balance)
+
+# 🔥 CAN be DIHACKED
+acc._balance = -1000
+print("Hacked:", acc._balance)
+
+"""
+Output: 
+
+Before: 100
+Hacked: -1000
+
+The state in this objcet can still be manipulation
+"""
