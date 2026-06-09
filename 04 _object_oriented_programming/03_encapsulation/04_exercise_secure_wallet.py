a59524413f21e5537ed62efea5d151a21255a922
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-09 09:34:54 +0700

    feat: create deposit method

--- 04 _object_oriented_programming/03_encapsulation/04_exercise_secure_wallet.py
+++ 04 _object_oriented_programming/03_encapsulation/04_exercise_secure_wallet.py
@@ -1,1 +1,25 @@
+class Wallet:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self._balance = balance
+
+    def get_balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        if amount <=0:
+            return False
+        self._balance +=amount
+        return True
+
+    def withdraw(self, amount):
+        # TODO: validasi
+        pass
+
+    def transfer(self, target_wallet, amount):
+        # TODO:
+        # 1. withdraw dari self
+        # 2. kalau gagal → stop
+        # 3. deposit ke target
+        pass
 
