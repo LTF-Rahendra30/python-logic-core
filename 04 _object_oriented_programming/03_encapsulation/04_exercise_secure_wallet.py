6a792ebb70b1930e41597e328a396c879ecd62c3
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-09 09:49:18 +0700

    feat: Try create object

--- 04 _object_oriented_programming/03_encapsulation/04_exercise_secure_wallet.py
+++ 04 _object_oriented_programming/03_encapsulation/04_exercise_secure_wallet.py
@@ -27,3 +27,17 @@ class Wallet:
         target_wallet.deposit(amount)
         return True
 
+# Test
+w1 = Wallet("Alice",100)
+w2 = Wallet("Bob",50)
+
+# get info
+print("Before:")
+print(f"wallet 1: {w1.get_balance()}, Wallet 2: {w2.get_balance()}")
+
+# Todo test transfer 
+w1.transfer(w2,30)
+
+# Get info
+print("After")
+print(f"wallet 1: {w1.get_balance()}, Wallet 2: {w2.get_balance()}")
